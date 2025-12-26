a1c89b7ccb8f302c5af0f6fda4e6261b38f6fcb0
SaydahbarovAzamjon <saydahbarovazamjon@gmail.com>
2025-12-26 23:24:02 +0900

    Bismillah: This is the portfolio ....

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ console.log("Web serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
-/* Bitimchi qadamga biz exress_dan kelayotgan malumotlardi kiritamiz.  */
+let user ;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
+
+
+/* Birinchi qadamga biz exress_dan kelayotgan malumotlardi kiritamiz.  */
 // 1: Kirish code
 app.use(express.static("public"));  // =>bu linr msnodi xammaga korinishi uchun;
 app.use(express.json());            //  kirib kelayotgan "express.js"  data_ni object korinishiga ogirib beradi
@@ -13,11 +24,18 @@ app.use(express.urlencoded({extended: true}));
 // 2 Session code
 
 // 3  Views code (BSSR) => traditional way
+
 app.set("views", "views");
-app.set("view engine", "ejs"); //BSSR backend server side rendring
+app.set("view engine", "ejs");              //BSSR backend server side rendring
+
+
 
 
 // 4 Routingcode
+
+app.get("/author", (req ,res) => {
+    res.render("author",{user: user})
+} );
 
 app.post("/create-item", (req ,res) => {
     console.log(req);
