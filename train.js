1cf0a8160ebc7a49b60f24918ec59954c750f7d4
SaydahbarovAzamjon <saydahbarovazamjon@gmail.com>
2025-12-30 02:34:54 +0900

    Training Area

--- train.js
+++ train.js
@@ -1,1 +1,23 @@
 console.log('TRAIN  AREA!')
+
+// TASK A
+
+// Harf sifatida kiritilgan birinchi parametr, 
+// kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
+// Funktsiya tuzing
+
+// Masalan: countLetter("e", "engineer")
+// 'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
+// 3 sonini qaytaradi
+
+
+function count (letter, word) {
+    return word.split("").filter(ele => ele === letter).length;
+}
+console.log(count("a", "Assalomu Alaykum"));
+
+function train (letter, word) {
+    return word.split("").filter(ele => ele === letter).length;
+}
+
+console.log(train("s", "Bismillah"));
