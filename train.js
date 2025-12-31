527d382d3f9e50e09d65a1a56fec0756ad7e143f
SaydahbarovAzamjon <saydahbarovazamjon@gmail.com>
2026-01-01 02:46:37 +0900

    TASK B

--- train.js
+++ train.js
@@ -1,4 +1,47 @@
 console.log('TRAIN  AREA!')
+// TASK B: 
+
+// Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
+// MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
+function string (str, num) {
+    let count = 0;
+    for(let symbl of str) {
+        if(symbl === String(num)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(string("ad2a54y79wet0sfgb9", "5"));
+
+
+
+
+
+// function string(str) {
+//     let count = 0;
+
+//     for (let symbl of str) {
+//     if (symbl >= '0' && symbl <= '9') {
+//         count++;
+//     }
+// }
+
+//         return count;
+// }
+
+// console.log(string("ad2a54y79wet0sfgb9")); // 7
+
+
+
+
+
+
+
+
+
+
 
 // TASK A
 
@@ -11,13 +54,13 @@ console.log('TRAIN  AREA!')
 // 3 sonini qaytaradi
 
 
-function count (letter, word) {
-    return word.split("").filter(ele => ele === letter).length;
-}
-console.log(count("a", "Assalomu Alaykum"));
+// function count (letter, word) {
+//     return word.split("").filter(ele => ele === letter).length;
+// }
+// console.log(count("a", "Assalomu Alaykum"));
 
-function train (letter, word) {
-    return word.split("").filter(ele => ele === letter).length;
-}
+// function train (letter, word) {
+//     return word.split("").filter(ele => ele === letter).length;
+// }
 
-console.log(train("s", "Bismillah"));
+// console.log(train("s", "Bismillah"));
