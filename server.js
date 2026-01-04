932e0375a7a4e0d69ee090a37e24764f997c0ef5
SaydahbarovAzamjon <saydahbarovazamjon@gmail.com>
2026-01-05 03:10:02 +0900

    New C task is solved

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ mongodb.connect(
             module.exports = client;
             const app = require("./app");
             const server = http.createServer(app);
+            
             let PORT = 3000;
             server.listen(PORT, function() {
             console.log(`The server is running succesfully on port: ${PORT},http://localhost:${PORT}`);
             });
         }
     });
-
+ 
--- train.js
+++ train.js
@@ -1,5 +1,35 @@
 console.log('TRAIN  AREA!');
 
+// TASK D : 
+
+// Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+
+// MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+
+function task(a, b) {
+    if (a.length !== b.length) return false;
+
+    const aa = a.split('').sort().join('');
+    const bb = b.split('').sort().join('');
+
+    return aa === bb;
+}
+
+
+
+const check = task("mit", "tim");    
+console.log(check)      // true
+
+
+
+
+
+
+
+
+
+
+
 
 // MITASK-C 
 
@@ -7,61 +37,40 @@ console.log('TRAIN  AREA!');
 // MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 
 //Yechim 
-const moment = require("moment");
-const time = moment().format("HH:mm:ss");
-    class Shop{
-            constructor(non, choy, shashlik) {
-        this.non = non;
-        this.choy = choy;
-        this.shashlik = shashlik;
-    }
-    qoldiq() {
-        const time = moment().format("HH:mm");
-        console.log(`Hozir ${time}da ${this.non}ta non, ${this.shashlik}ta shashlik va ${this.choy}ta choy mavjud!`);
-    }
+// const moment = require("moment");
+// const time = moment().format("HH:mm:ss");
+//     class Shop{
+//             constructor(non, choy, shashlik) {
+//         this.non = non;
+//         this.choy = choy;
+//         this.shashlik = shashlik;
+//     }
+//     qoldiq() {
+//         const time = moment().format("HH:mm");
+//         console.log(`Hozir ${time}da ${this.non}ta non, ${this.shashlik}ta shashlik va ${this.choy}ta choy mavjud!`);
+//     }
 
-    sotish(mahsulot, miqdor ) {
-        const time = moment().format("HH:mm:ss");
-        if (this[mahsulot] >= miqdor){
-            this[mahsulot] -= miqdor;
-            console.log(`${time}da ${miqdor}ta ${mahsulot} sotildi}`)
-        } else {
-            console.log(`${time}da yetarli ${mahsulot} yo'q!`)
-        }
-    }
+//     sotish(mahsulot, miqdor ) {
+//         const time = moment().format("HH:mm:ss");
+//         if (this[mahsulot] >= miqdor){
+//             this[mahsulot] -= miqdor;
+//             console.log(`${time}da ${miqdor}ta ${mahsulot} sotildi}`)
+//         } else {
+//             console.log(`${time}da yetarli ${mahsulot} yo'q!`)
+//         }
+//     }
 
-    qabul(mahsulot, soni) {
-    const time = moment().format("HH:mm");
-    this[mahsulot] += soni;
-    console.log(`${time}da ${soni}ta ${mahsulot} qabul qilindi`);
-    }
-}
+//     qabul(mahsulot, miqdor) {
+//     const time = moment().format("HH:mm");
+//     this[mahsulot] += miqdor;
+//     console.log(`${time}da ${soni}ta ${mahsulot} qabul qilindi`);
+//     }
+// }
 
-const order = new Shop(4, 6, 7)
-order.qoldiq();
-order.sotish('shashlik', 5);
-order.qoldiq()    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const order = new Shop(4, 6, 7)
+// order.qoldiq();
+// order.sotish('shashlik', 5);
+// order.qoldiq()    
 
 
 // TASK B: 
